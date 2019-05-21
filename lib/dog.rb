b9636753a8d063b5787bbcaf4c85acae7ae9f928
Adella <adellakenn@gmail.com>
2019-05-21 21:20:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,14 @@
 class Dog 
-  def name=(dog_name)
+  def name=(dog_name)           # name equals method
+    this_dogs_name = dog_name   # takes in an argument of a dog's name and let is equal a variable  
+  end
+  
+  def name
+    this_dogs_name      # responisble for reporting the name 
+  end
+end
+
+lassie = Dog.new 
+lassie.name = "Lassie"
 
 
